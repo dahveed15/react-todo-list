2e74f3b83238f383de994e59d4f9ed1d8f908642
Dahlton Seyler <dseyler@samcart.com>
2023-06-02 15:33:31 -0400

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:.idea/workspace.xml

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,13 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const TodoForm = () => {
-    useEffect(() => {
-        console.log('TODO component rendered!');
-    }, [])
+    const [todo, setTodo] = useState("Hello");
+    // useEffect(() => {
+    //     console.log('TODO component rendered!');
+    // }, [])
     return (
         <div>
             <form>
-                <input type="text" name="todo" />
+                <input type="text" name="todo" value={todo} onChange={() => console.log("TODO")} />
             </form>
         </div>
     );

7c49f75cbfee153be5ed5d61be2f1be710d43cd0
Marco Pineda <mapineda1222@gmail.com>
2023-06-02 14:54:59 -0500

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/components/TodoForm.jsx

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
 
 const TodoForm = () => {
-    const [todo, setTodo] = useState("");
-    // useEffect(() => {
-    //     console.log('TODO component rendered!');
-    // }, [])
+    const [todo, setTodo] = useState({
+        task: "",
+        completed: false
+    });
 
-    const handleInputChange = () => {
-        
+
+    const handleInputChange = (e) => {
+        setTodo({
+            ...todo,
+            task: e.target.value
+        });
     }
 
     return (
         <div>
             <form>
-                <input type="text" name="todo" value={todo} onChange={() => console.log("TODO")} />
+                <input type="text" name="todo" value={todo.task} onChange={handleInputChange} />
+                <button type="submit" onClick={()=> console.log(todo.task)}>Submit</button>
             </form>
         </div>
     );

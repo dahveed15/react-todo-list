3ff7262864ae908972311f8fb7621ab22eed43b4
mikezm <mmarshall@samcart.com>
2023-06-23 16:34:13 -0400

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/components/TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,7 +2,12 @@ const TodoList = (props) => {
     return (
         <div>
             { props.todos.map(todo => {
-                return <div><p>{todo.task}</p></div>
+                return (
+                    <div key={todo.id}>
+                        <p>{todo.task}</p>
+                        <button onClick={() => props.removeTodo(todo.id)}>X</button>
+                    </div>
+                )
             }) }
         </div>
     )
